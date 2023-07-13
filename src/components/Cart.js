bd4ecac78accb4138132acf33b20608c05fb3ecc
Helaine Ribeiro <helainecrcosta@gmail.com>
2023-07-13 07:32:57 -0300

    alterando arquivo cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,27 +1,3 @@
-/* import React from 'react';
-
-const Cart = ({ cartItems, removeFromCart }) => {
-  return (
-    <div className="cart-items">
-      <h2>Carrinho de Compras</h2>
-      <h3>Itens no Carrinho:</h3>
-      {cartItems.map((item) => (
-        <div key={item.product.id} className="cart-item-lista">
-          <div>
-            {/* aparece o nome e descrição do produto /}
-            <p>{item.product.name}</p>
-            <p>{item.product.description}</p>
-          </div>
-          <p>Quantidade: {item.quantity}</p>
-          <button onClick={() => removeFromCart(item)}>Remover</button>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Cart; */
-
 import React from 'react';
 
 const Cart = ({ cartItems, removeFromCart }) => {
